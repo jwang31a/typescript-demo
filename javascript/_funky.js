a37c8fdbe5c1f67019841f4f8094dec5294d2257
Your Name <you@example.com>
2023-03-31 19:09:56 -0400

    classes

---
+++ javascript/_funky.js
@@ -0,0 +1,14 @@
+var a = 2;
+var b = "2"
+
+console.log(a + b);
+console.log(a - b);
+console.log("\n")
+
+var l = {};
+var o = [];
+
+console.log(l + l);
+console.log(l + o);
+console.log(o + l);
+console.log(o + o);
